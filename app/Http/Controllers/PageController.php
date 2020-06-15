87da47fb4cdd00270f3aca178081b91e2c746027
Ілюшин Олена Сергіївна <ilus_spw8@student.itstep.org>
2020-06-15 15:03:58 +0300

    Create all pages

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -9,4 +9,34 @@ class PageController extends Controller
     public function index(){
         return view('pages.home');
     }
+    public function about_us(){
+        return view('pages.about_us');
+    }
+    public function course_grid_2(){
+        return view('pages.course_grid_2');
+    }
+    public function course_grid_3(){
+        return view('pages.course_grid_3');
+    }
+    public function course_grid_4(){
+        return view('pages.course_grid_4');
+    }
+    public function blog(){
+        return view('pages.blog');
+    }
+    public function blog_single(){
+        return view('pages.blog_single');
+    }
+    public function contact(){
+        return view('pages.contact');
+    }
+    public function pricing(){
+        return view('pages.pricing');
+    }
+    public function teachers(){
+        return view('pages.teachers');
+    }
+
+
+    
 }
--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'PageController@index')->name("post.index");
+Route::get('/', 'PageController@index')->name("pages.index");
+Route::get('/about_us', 'PageController@about_us')->name("pages.about_us");
+Route::get('/course_grid_2', 'PageController@course_grid_2')->name("pages.course_grid_2");
+Route::get('/course_grid_3', 'PageController@course_grid_3')->name("pages.course_grid_3");
+Route::get('/course_grid_4', 'PageController@course_grid_4')->name("pages.course_grid_4");
+Route::get('/blog', 'PageController@blog')->name("pages.blog");
+Route::get('/blog_single', 'PageController@blog_single')->name("pages.blog_single");
+Route::get('/contact', 'PageController@contact')->name("pages.contact");
+Route::get('/pricing', 'PageController@pricing')->name("pages.pricing");
+Route::get('/teachers', 'PageController@teachers')->name("pages.teachers");
+//Route::get('/about_us', 'PageController@about_us')->name("pages.about_us");
+
+
+
+
 
 
 
